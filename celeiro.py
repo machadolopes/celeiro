002a40ce1ca65e79b36620a1a71b01b4aff2c67b
machadolopes <marcel.felipe@gmail.com>
2019-12-09 18:49:52 -0300

    second commmit

--- celeiro.py
+++ celeiro.py
@@ -8,12 +8,22 @@ today = date.today()
 dia = today.strftime("%d")
 mes = today.strftime("%m") 
 ano = today.strftime("%Y")
+#seleciona caderno com base no dia mês e ano atual
 caderno = 'https://www.jusbrasil.com.br/diarios/documentos/DJRJ/'+ano+'/'+mes+'/'+dia+'/Iii-judicial-1a-instancia-capital/'
 driver.get(caderno)
-documentos = driver.find_elements_by_class_name('number')
-ultima_pagina = int(documentos[-1].text)
-
-
+#conta total de páginas no caderno
+total_paginas = driver.find_elements_by_class_name('number')
+#calcula a última página do caderno
+ultima_pagina = int(total_paginas[-1].text)
+#seleciona documentos da página
+documentos = driver.find_elements_by_class_name('journal-block-title')
+lista_documentos = ()
+for docs in documentos:
+    docs.click()
+    documento_completo = driver.find_element_by_class_name('DocumentView-content document-content fos-bottomref')
+    documentos_info = driver.find_element_by_class_name('DocumentView-content-text')
+    lista_documentos.append(documentos_info)
+print (lista_documentos)
 driver.close()
 
 
